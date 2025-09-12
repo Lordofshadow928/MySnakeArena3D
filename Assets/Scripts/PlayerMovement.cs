35a1762bae5ad2b0fcf2cd10fb4eb68c13090c05
Lordofshadow928 <flairdante@gmail.com>
2025-09-12 21:30:18 +0700

    nope

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -31,6 +31,8 @@ public class PlayerMovement : MonoBehaviour
         growSnake();
         growSnake();
         growSnake();
+        growSnake();
+        growSnake();
         tailMove();
     }
     public void FixedUpdate()
