e8ab9715729d98965de2553ec7a168de57026e37
Lordofshadow928 <flairdante@gmail.com>
2026-03-31 16:52:54 +0700

    try to fix spawn snake but not work

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -139,12 +139,12 @@ public class PlayerMovement : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Food"))
+        if (other.CompareTag("Food"))
         {
             GrowSnake();
             other.gameObject.SetActive(false);
         }
-            
+
     }
     private void OnDrawGizmos()
     {
@@ -155,3 +155,159 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 }
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class PlayerMovement : MonoBehaviour
+//{
+//    [Header("Movement Settings")]
+//    public float moveSpeed = 5.0f;
+//    public float steerSpeed = 180.0f;
+
+//    [Header("Body Settings")]
+//    public float bodySpeed = 5.0f;
+//    [SerializeField] private int gap = 5;
+
+//    [Header("Prefabs")]
+//    public GameObject bodyPrefab;
+//    public GameObject tailPrefab;
+
+//    // Internal
+//    private List<GameObject> bodyParts = new List<GameObject>();
+//    private List<Vector3> positionHistory = new List<Vector3>();
+
+//    private void Start()
+//    {
+//        Init();
+//    }
+
+//    [ContextMenu("Init")]
+//    private void Init()
+//    {
+//        positionHistory.Clear();
+//        bodyParts.Clear();
+
+//        PrefillPositionHistory();
+//        SpawnSnake();
+//    }
+
+//    private void SpawnSnake()
+//    {
+//        for (int i = 0; i < 5; i++)
+//        {
+//            GrowSnake();
+//        }
+
+//        AddTail();
+//    }
+
+//    private void PrefillPositionHistory()
+//    {
+//        for (int i = 0; i < 100; i++)
+//        {
+//            positionHistory.Add(transform.position - transform.forward * 0.1f * i);
+//        }
+//    }
+
+//    private void FixedUpdate()
+//    {
+//        MoveSnake();
+//    }
+
+//    private void MoveSnake()
+//    {
+//        MoveHead();
+
+//        // record AFTER moving
+//        positionHistory.Insert(0, transform.position);
+
+//        PruneHistory();
+//        MoveBody();
+//    }
+
+//    private void MoveHead()
+//    {
+//        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+//        float steer = Input.GetAxis("Horizontal");
+//        transform.Rotate(Vector3.up * steer * steerSpeed * Time.deltaTime);
+//    }
+
+//    private void PruneHistory()
+//    {
+//        int maxHistory = (bodyParts.Count + 1) * gap;
+
+//        if (positionHistory.Count > maxHistory)
+//        {
+//            positionHistory.RemoveRange(maxHistory, positionHistory.Count - maxHistory);
+//        }
+//    }
+
+//    private void MoveBody()
+//    {
+//        for (int i = 0; i < bodyParts.Count; i++)
+//        {
+//            int index = Mathf.Min(i * gap, positionHistory.Count - 1);
+
+//            Vector3 targetPos = positionHistory[index];
+//            Transform part = bodyParts[i].transform;
+
+//            Vector3 dir = targetPos - part.position;
+
+//            part.position += dir * bodySpeed * Time.deltaTime;
+
+//            if (dir.sqrMagnitude > 0.001f)
+//            {
+//                part.rotation = Quaternion.LookRotation(dir);
+//            }
+//        }
+//    }
+
+//    private void GrowSnake()
+//    {
+//        Vector3 spawnPos = GetLastBodyPosition();
+
+//        GameObject body = Instantiate(bodyPrefab, spawnPos, Quaternion.identity);
+//        bodyParts.Add(body);
+//    }
+
+//    private void AddTail()
+//    {
+//        Vector3 spawnPos = GetLastBodyPosition();
+
+//        GameObject tail = Instantiate(tailPrefab, spawnPos, Quaternion.identity);
+//        bodyParts.Add(tail);
+//    }
+
+//    private Vector3 GetLastBodyPosition()
+//    {
+//        if (bodyParts.Count == 0)
+//            return transform.position - transform.forward;
+
+//        Transform last = bodyParts[bodyParts.Count - 1].transform;
+//        return last.position - last.forward;
+//    }
+
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Food"))
+//        {
+//            GrowSnake();
+
+//            // disable instead of destroy
+//            other.gameObject.SetActive(false);
+//        }
+//    }
+
+//    private void OnDrawGizmos()
+//    {
+//        Gizmos.color = Color.red;
+
+//        foreach (var pos in positionHistory)
+//        {
+//            Gizmos.DrawSphere(pos, 0.05f);
+//        }
+//    }
+//}
