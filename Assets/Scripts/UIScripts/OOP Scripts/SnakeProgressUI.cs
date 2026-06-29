3130ec6e3918913c3c8d95e98a34ba2372c3a07b
Lordofshadow928 <flairdante@gmail.com>
2026-06-29 16:10:31 +0700

    add best progress text

--- Assets/Scripts/UIScripts/OOP Scripts/SnakeProgressUI.cs
+++ Assets/Scripts/UIScripts/OOP Scripts/SnakeProgressUI.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SnakeProgressUI : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private SnakeEnergy energy;
     [SerializeField] private TMP_Text progressText;
+    [SerializeField] private TMP_Text bestProgressText;
     [SerializeField] private Image fillImage;
     [SerializeField] private ResultHandler result;
     private bool hasWon;
+    private string progressKey;
+    private int bestPercent;
+
+    private void Start()
+    {
+        progressKey = $"LevelBest_{SceneManager.GetActiveScene().buildIndex}";
+
+        bestPercent = PlayerPrefs.GetInt(progressKey, 0);
+        bestProgressText.text = $"Best {bestPercent}%";
+    }
     private void OnEnable()
     {
         energy.OnEnergyChanged += UpdateUI;
@@ -29,6 +41,15 @@ public class SnakeProgressUI : MonoBehaviour
         fillImage.fillAmount = percent;
 
         progressText.text = $"{Mathf.RoundToInt(percent * 100)}%";
+        int currentPercent = Mathf.RoundToInt(percent * 100);
+        if (currentPercent > bestPercent)
+        {
+            bestPercent = currentPercent;
+            PlayerPrefs.SetInt(progressKey, bestPercent);
+            PlayerPrefs.Save();
+
+            bestProgressText.text = $"Best {bestPercent}%";
+        }
 
         if (!hasWon && percent >= 1f)
         {
